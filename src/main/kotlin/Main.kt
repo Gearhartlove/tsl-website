51bad2dbdca26e56da1a725e8fb2829358da6332
Kristoff Finley <kristoff.finley@quiq.com>
2024-02-13 23:03:28 -0700

    #NP add htmx + starting template

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,27 @@
 import io.javalin.Javalin
 import io.javalin.community.ssl.SslPlugin
+import java.io.File
 
-val sslPlugin = SslPlugin { conf ->
-    conf.pemFromPath(
-        "/etc/letsencrypt/live/twoshotslater.com/fullchain.pem",
-        "/etc/letsencrypt/live/twoshotslater.com/privkey.pem"
-    )
+fun inDebugMode(): Boolean {
+    return System.getenv("DEBUG") == "true"
 }
 
 fun main(args: Array<String>) {
-    val app = Javalin.create { javalinConfig ->
-        javalinConfig.registerPlugin(sslPlugin)
+    val _app = Javalin.create { javalinConfig ->
+        if (!inDebugMode()) {
+            val sslPlugin = SslPlugin { conf ->
+                conf.pemFromPath(
+                    "/etc/letsencrypt/live/twoshotslater.com/fullchain.pem",
+                    "/etc/letsencrypt/live/twoshotslater.com/privkey.pem"
+                )
+            }
+            javalinConfig.registerPlugin(sslPlugin)
+        }
     }
-        .get("/") { ctx -> ctx.result("Hello World") }
+        .get("/") { ctx ->
+            val landingPage = File("src/main/resources/foo.html")
+            ctx.html(landingPage.readText())
+        }
         .get("/foo") { ctx -> ctx.result("bar") }
         .start(443)
 }
